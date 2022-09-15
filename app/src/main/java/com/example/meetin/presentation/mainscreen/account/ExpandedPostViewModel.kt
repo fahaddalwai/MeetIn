a4b9b689653797a061f7872c379fbd94f52ecb49
dalwa <dalwaifahad@gmail.com>
2022-09-15 20:24:58 +0530

    feat:expanded post fragment created

---
+++ app/src/main/java/com/example/meetin/presentation/mainscreen/account/ExpandedPostViewModel.kt
@@ -0,0 +1,42 @@
+package com.example.meetin.presentation.mainscreen.account
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.meetin.domain.model.Post
+import com.example.meetin.domain.repository.Repository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ExpandedPostViewModel @Inject constructor(private val repository: Repository, state: SavedStateHandle) : ViewModel() {
+
+    private val _caption = MutableLiveData<String>()
+    val caption: LiveData<String>
+        get() = _caption
+
+    private val _postUrl = MutableLiveData<String>()
+    val postUrl: LiveData<String>
+        get() = _postUrl
+
+    private val _postLikes = MutableLiveData<Int>()
+    val postLikes: LiveData<Int>
+        get() = _postLikes
+
+    init{
+        state.get<String>("url")?.let {
+            _postUrl.value=it
+        }
+
+        state.get<Int>("likes")?.let {
+            _postLikes.value=it
+        }
+
+        state.get<String>("caption")?.let {
+            _caption.value= "$it Likes"
+        }
+
+    }
+}
