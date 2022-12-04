0930fa60b9c36b08d2d650ead8996613cbd6fabb
Fahad Dalwai <71600359+fahaddalwai@users.noreply.github.com>
2022-12-04 18:20:16 +0530

    Delete Constant.kt

--- app/src/main/java/com/isavit/meetin/core/util/Constant.kt
+++
@@ -1,7 +0,0 @@
-package com.isavit.meetin.core.util
-
-object Constants {
-    const val GOOGLE_CLIENT_ID = "865090821364-vja6esltf9jin68gj4rp6125u67o6gfg.apps.googleusercontent.com"
-    const val RC_SIGN_IN = 1001
-    const val USER_PREF_NAME="user"
-}
